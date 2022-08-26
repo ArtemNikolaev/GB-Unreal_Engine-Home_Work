7cf9208db7fd19fdf4d1ade726f3ba726d65afce
Artem Nikolaev <artem@nikolaev.by>
2022-08-26 12:24:54 +0300

    Levels

---
+++ Source/MyProject/TankPawn.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/Pawn.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "TankPawn.generated.h"
+
+class UStaticMeshComponent;
+
+UCLASS()
+class MYPROJECT_API ATankPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ATankPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category="Components")
+	UStaticMeshComponent* BodyMesh;
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category="Components")
+	UStaticMeshComponent* TurretMesh;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Movement|Speed")
+	float MoveSpeed = 100;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Movement|Speed")
+	float RotationSpeed = 100;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category="Components")
+	USpringArmComponent * SpringArm;
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category="Components")
+	UCameraComponent * Camera;
+
+	float _targetForwardAxisValue;
+
+public:
+	UFUNCTION()
+	void MoveForward(float AxisValue);
+	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
